870c6fe4cc9669ca250592c991f5eacf4d6d03ab
Mitxh13 <kmitesh2006@gmail.com>
2026-01-02 16:07:48 +0530

    updating files

--- authentication.py
+++ authentication.py
@@ -1,2 +1,17 @@
-def hello():
-    print("Hello")
+def NewUser():
+    Ufp = open("Ucredentials\users.csv")
+    Ulf = "Ucredentials\logfile.txt"
+    UserName = input("Enter your name: ")
+    UserId = input("Create your user ID: ")
+    found = 0
+    
+
+
+
+
+
+def login():
+    Userid = input("Enter your user ID: ")
+    Userpassword = input("Enter your Password: ")
+    Check = 0
+    
--- main.py
+++ main.py
@@ -2,16 +2,27 @@ import authentication
 print("Welcome to Checkly!!")
 print("********************\n")
 
+def main_dashboard():
+    print("waiting")
+
+#This function is for inital start of CLI-APP user needs to login to move further
 def landing():
     print("Please Login to continue to main dashboard!")
-    print("1.Login to checkly \n 2.Create an New account")
+    print("1.Login to checkly \n2.Create an New account \n3.Close the program")
     choice_login = int(input("Enter your Preference: "))
     match(choice_login):
         case(1):
             print("---redirecting to login page---")
-            authentication.login()
+            if(authentication.login()):
+                main_dashboard()
+            else:
+                print("User credentials  are invaild!")
+                print("retry or close the program")
+                landing()
         case(2):
             print("---redirecting to Account Creation page---")
             authentication.NewUser()
         case(3):
             print("Closing the program safely!!")
+
+landing()
